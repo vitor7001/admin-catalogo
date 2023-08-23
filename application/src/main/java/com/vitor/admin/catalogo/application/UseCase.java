14454de90035709f0bb508e191f2ce710e477283
vitor7001 <vitorgomes7001@gmail.com>
2023-08-22 22:58:53 -0300

    feat: setup inicial

---
+++ application/src/main/java/com/vitor/admin/catalogo/application/UseCase.java
@@ -0,0 +1,9 @@
+package com.vitor.admin.catalogo.application;
+
+import com.vitor.admin.catalogo.domain.Category;
+
+public class UseCase {
+    public Category execute(){
+        return new Category();
+    }
+}
---
+++ domain/src/main/java/com/vitor/admin/catalogo/domain/Category.java
@@ -0,0 +1,5 @@
+package com.vitor.admin.catalogo.domain;
+
+public class Category {
+   public String id;
+}

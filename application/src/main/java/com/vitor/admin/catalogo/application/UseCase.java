7e5ebcf646243310c3de03813b5aaa6db67e5a45
vitor7001 <vitorgomes7001@gmail.com>
2023-08-30 19:44:53 -0300

    test: deve criar uma categoria

--- application/src/main/java/com/vitor/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/vitor/admin/catalogo/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.vitor.admin.catalogo.application;
 
-import com.vitor.admin.catalogo.domain.Category;
-
 public class UseCase {
     public Category execute(){
         return new Category();

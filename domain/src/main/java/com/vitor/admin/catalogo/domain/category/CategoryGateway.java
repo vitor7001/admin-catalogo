3e9857697557470780c979d29c87eb9ba8ffa5f0
vitor7001 <vitorgomes7001@gmail.com>
2023-09-08 18:32:49 -0300

    feat: crianmdo interface do gateway de category

---
+++ domain/src/main/java/com/vitor/admin/catalogo/domain/category/CategoryGateway.java
@@ -0,0 +1,14 @@
+package com.vitor.admin.catalogo.domain.category;
+
+import com.vitor.admin.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+    void deleteById(CategoryID anId);
+    Optional<Category> findById(CategoryID anId);
+    Category update(CategoryID aCategory);
+    Pagination<Category> findAll(CategorySearchQuery aQuery);
+}
---
+++ domain/src/main/java/com/vitor/admin/catalogo/domain/pagination/Pagination.java
@@ -0,0 +1,12 @@
+package com.vitor.admin.catalogo.domain.pagination;
+
+import java.util.List;
+
+public record Pagination<T>(
+        int currentPage,
+        int perPage,
+        long total,
+        List<T> items
+) {
+
+}

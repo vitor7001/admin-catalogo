ab5b4629ce716943a25037c246d94ec6a4d4ecfc
vitor7001 <vitorgomes7001@gmail.com>
2023-09-29 20:10:36 -0300

    setup: hooks clean mocks

--- application/src/test/java/com/vitor/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/vitor/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -4,6 +4,7 @@ import com.vitor.admin.catalogo.domain.category.CategoryGateway;
 import com.vitor.admin.catalogo.domain.exceptions.DomainException;
 import com.vitor.admin.catalogo.domain.validation.handler.Notification;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.AdditionalAnswers;
@@ -28,6 +29,11 @@ public class CreateCategoryUseCaseTest {
 
     @Mock
     private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp(){
+        Mockito.reset(categoryGateway);
+    }
 
     @Test
     public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
@@ -58,7 +64,6 @@ public class CreateCategoryUseCaseTest {
                         }
                 ));
     }
-
 
     @Test
     public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnDomainException(){
--- application/src/test/java/com/vitor/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
+++ application/src/test/java/com/vitor/admin/catalogo/application/category/update/UpdateCategoryUseCaseTest.java
@@ -6,6 +6,7 @@ import com.vitor.admin.catalogo.domain.category.CategoryGateway;
 import com.vitor.admin.catalogo.domain.category.CategoryID;
 import com.vitor.admin.catalogo.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -29,6 +30,11 @@ public class UpdateCategoryUseCaseTest {
 
     @Mock
     private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp(){
+        Mockito.reset(categoryGateway);
+    }
 
     @Test
     public void givenAValidCommand_whenCallsUpdateCategory_shouldReturnCategoryId() {

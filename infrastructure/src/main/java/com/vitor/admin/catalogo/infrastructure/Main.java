fd0c607b92fd82d5f55e202d53047d822e30ea29
vitor7001 <vitorgomes7001@gmail.com>
2023-10-10 22:27:32 -0300

    setup: profiles springboot

--- infrastructure/src/main/java/com/vitor/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/vitor/admin/catalogo/infrastructure/Main.java
@@ -4,11 +4,14 @@ import com.vitor.admin.catalogo.application.UseCase;
 import com.vitor.admin.catalogo.infrastructure.configuration.WebServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServiceConfig.class ,args);
     }
 }

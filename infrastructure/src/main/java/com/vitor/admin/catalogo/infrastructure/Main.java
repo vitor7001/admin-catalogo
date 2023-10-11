f1ece53a272d8fb6c02c7f054a2d90a433b885cc
vitor7001 <vitorgomes7001@gmail.com>
2023-10-10 21:17:09 -0300

    setup: adicionando springboot na camada de infraestrutura

--- infrastructure/src/main/java/com/vitor/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/vitor/admin/catalogo/infrastructure/Main.java
@@ -1,10 +1,14 @@
 package com.vitor.admin.catalogo.infrastructure;
 
 import com.vitor.admin.catalogo.application.UseCase;
+import com.vitor.admin.catalogo.infrastructure.configuration.WebServiceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
+        SpringApplication.run(WebServiceConfig.class ,args);
     }
 }
---
+++ infrastructure/src/main/java/com/vitor/admin/catalogo/infrastructure/configuration/WebServiceConfig.java
@@ -0,0 +1,9 @@
+package com.vitor.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.vitor.admin.catalogo")
+public class WebServiceConfig {
+}

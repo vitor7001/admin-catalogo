7bef9544f17d9f199a49818a22373fdaec526ec8
vitor7001 <vitorgomes7001@gmail.com>
2023-10-12 18:56:29 -0300

    feat: entidade jpa de categoria

--- domain/src/main/java/com/vitor/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/vitor/admin/catalogo/domain/category/Category.java
@@ -44,6 +44,39 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable{
         return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
     }
 
+    public static Category with(final Category aCategory) {
+        return with(
+                aCategory.getId(),
+                aCategory.name,
+                aCategory.description,
+                aCategory.isActive(),
+                aCategory.createdAt,
+                aCategory.updatedAt,
+                aCategory.deletedAt
+        );
+    }
+
+    public static Category with(
+            final CategoryID anId,
+            final String name,
+            final String description,
+            final boolean active,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final Instant deletedAt
+    ) {
+        return new Category(
+                anId,
+                name,
+                description,
+                active,
+                createdAt,
+                updatedAt,
+                deletedAt
+        );
+    }
+
+
     @Override
     public void validate(final ValidationHandler handler) {
         new CategoryValidator(this, handler).validate();

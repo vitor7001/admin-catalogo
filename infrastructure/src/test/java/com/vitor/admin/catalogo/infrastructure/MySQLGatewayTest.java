5eb0903a0575cf7f3b0f8d61e6d9f8018f3b80a9
vitor7001 <vitorgomes7001@gmail.com>
2023-10-12 20:18:09 -0300

    setup: estrategia para testes integrados com o banco de dados

---
+++ infrastructure/src/test/java/com/vitor/admin/catalogo/infrastructure/MySQLGatewayTest.java
@@ -0,0 +1,40 @@
+package com.vitor.admin.catalogo.infrastructure;
+
+import com.vitor.admin.catalogo.infrastructure.category.CategoryMySQLGatewayTest;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.lang.annotation.*;
+import java.util.Collection;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test")
+@ComponentScan(includeFilters = {
+        @ComponentScan.Filter(type= FilterType.REGEX, pattern = ".*[MySQLGateway]")
+})
+@DataJpaTest
+@ExtendWith(MySQLGatewayTest.CleanUpExtension.class)
+public @interface MySQLGatewayTest {
+
+    class CleanUpExtension implements BeforeEachCallback {
+        @Override
+        public void beforeEach(ExtensionContext context) throws Exception {
+            final var repositories = SpringExtension.getApplicationContext(context)
+                    .getBeansOfType(CrudRepository.class)
+                    .values();
+        }
+
+        private void cleanUp(final Collection<CrudRepository> repositories){
+            repositories.forEach(CrudRepository::deleteAll);
+        }
+    }
+}
---
+++ infrastructure/src/test/java/com/vitor/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -0,0 +1,37 @@
+package com.vitor.admin.catalogo.infrastructure.category;
+
+import com.vitor.admin.catalogo.infrastructure.MySQLGatewayTest;
+import com.vitor.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collection;
+
+@MySQLGatewayTest
+public class CategoryMySQLGatewayTest {
+
+    @Autowired
+    private CategoryMySQLGateway categoryMySQLGateway;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testInjectedDependecies(){
+        Assertions.assertNotNull(categoryMySQLGateway);
+        Assertions.assertNotNull(categoryRepository);
+
+    }
+
+}

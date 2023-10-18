ffce46ba9ecb1fd71e27d2ef8e1784923f4a8cbb
vitor7001 <vitorgomes7001@gmail.com>
2023-10-18 20:22:01 -0300

    test: método create do gateway de categoria

--- infrastructure/src/test/java/com/vitor/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/vitor/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,6 +1,8 @@
 package com.vitor.admin.catalogo.infrastructure.category;
 
+import com.vitor.admin.catalogo.domain.category.Category;
 import com.vitor.admin.catalogo.infrastructure.MySQLGatewayTest;
+import com.vitor.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import com.vitor.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,16 +24,44 @@ import java.util.Collection;
 public class CategoryMySQLGatewayTest {
 
     @Autowired
-    private CategoryMySQLGateway categoryMySQLGateway;
+    private CategoryMySQLGateway categoryGateway;
 
     @Autowired
     private CategoryRepository categoryRepository;
 
     @Test
-    public void testInjectedDependecies(){
-        Assertions.assertNotNull(categoryMySQLGateway);
-        Assertions.assertNotNull(categoryRepository);
+    public void givenAValidCategory_whenCallsCreate_shouldReturnANewCategory(){
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive =  true;
 
+        final var aCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var actualCategory = categoryGateway.create(aCategory);
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        Assertions.assertEquals(aCategory.getId(), actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualCategory.getCreatedAt());
+        Assertions.assertEquals(aCategory.getUpdatedAt(), actualCategory.getUpdatedAt());
+        Assertions.assertEquals(aCategory.getDeletedAt(), actualCategory.getDeletedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+
+        final var actualEntity = categoryRepository.findById(aCategory.getId().getValue()).get();
+
+        Assertions.assertEquals(aCategory.getId().getValue(), actualEntity.getId());
+        Assertions.assertEquals(expectedName, actualEntity.getName());
+        Assertions.assertEquals(expectedDescription, actualEntity.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualEntity.isActive());
+        Assertions.assertEquals(aCategory.getCreatedAt(), actualEntity.getCreatedAt());
+        Assertions.assertEquals(aCategory.getUpdatedAt(), actualEntity.getUpdatedAt());
+        Assertions.assertEquals(aCategory.getDeletedAt(), actualEntity.getDeletedAt());
+        Assertions.assertNull(actualEntity.getDeletedAt());
     }
 
 }

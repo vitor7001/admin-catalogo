e0118a6258b312a913414aa684b7d88529f77b77
vitor7001 <vitorgomes7001@gmail.com>
2023-10-18 20:42:22 -0300

    test: category repository cenários not nullable

--- domain/src/main/java/com/vitor/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/vitor/admin/catalogo/domain/category/Category.java
@@ -4,6 +4,7 @@ import com.vitor.admin.catalogo.domain.AggregateRoot;
 import com.vitor.admin.catalogo.domain.validation.ValidationHandler;
 
 import java.time.Instant;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Category extends AggregateRoot<CategoryID> implements Cloneable{
@@ -28,8 +29,8 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable{
         this.name = aName;
         this.description = aDescription;
         this.active = isActive;
-        this.createdAt = aCreationDate;
-        this.updatedAt = aUpdateDate;
+        this.createdAt = Objects.requireNonNull(aCreationDate, "'createdAt' should not be null");
+        this.updatedAt = Objects.requireNonNull(aUpdateDate, "'updatedAt' should not be null");
         this.deletedAt = aDeleteDate;
     }
 

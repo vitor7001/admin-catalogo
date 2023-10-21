31db30abad7a24f33dd897ad58a5699a4ad66774
vitor7001 <vitorgomes7001@gmail.com>
2023-10-21 09:49:27 -0300

    test: delete by id de categoria

--- infrastructure/src/test/java/com/vitor/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
+++ infrastructure/src/test/java/com/vitor/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -1,6 +1,7 @@
 package com.vitor.admin.catalogo.infrastructure.category;
 
 import com.vitor.admin.catalogo.domain.category.Category;
+import com.vitor.admin.catalogo.domain.category.CategoryID;
 import com.vitor.admin.catalogo.infrastructure.MySQLGatewayTest;
 import com.vitor.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import com.vitor.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
@@ -100,5 +101,31 @@ public class CategoryMySQLGatewayTest {
         Assertions.assertTrue(aCategory.getUpdatedAt().isBefore(actualCategory.getUpdatedAt()));
         Assertions.assertEquals(aCategory.getDeletedAt(), actualEntity.getDeletedAt());
         Assertions.assertNull(actualEntity.getDeletedAt());
+    }
+
+    @Test
+    public void givenAPrePersistedCategoryAndValidCategoryId_whenTryToDeleteIt_shouldDeleteCategory(){
+        final var aCategory = Category.newCategory("Filmes", null, true);
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        categoryRepository.saveAndFlush(CategoryJpaEntity.from(aCategory));
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        categoryGateway.deleteById(aCategory.getId());
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+    }
+
+    @Test
+    public void givenAPrePersistedCategoryAndInvalidCategoryId_whenTryToDeleteIt_shouldDeleteCategory(){
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        categoryGateway.deleteById(CategoryID.from("invalid"));
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
     }
 }

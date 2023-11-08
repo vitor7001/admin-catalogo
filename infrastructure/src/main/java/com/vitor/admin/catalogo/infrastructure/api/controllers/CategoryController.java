ca6d983f225a08f765251b6f86aef594df2275c2
vitor7001 <vitorgomes7001@gmail.com>
2023-11-08 20:55:11 -0300

    setup: estrategia para testes integrados de controller

--- infrastructure/src/main/java/com/vitor/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/vitor/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -1,12 +1,22 @@
 package com.vitor.admin.catalogo.infrastructure.api.controllers;
 
+import com.vitor.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import com.vitor.admin.catalogo.domain.pagination.Pagination;
 import com.vitor.admin.catalogo.infrastructure.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class CategoryController implements CategoryAPI {
+
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(final CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
+    }
+
     @Override
     public ResponseEntity<?> createCategory() {
         return null;
